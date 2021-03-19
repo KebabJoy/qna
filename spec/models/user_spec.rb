8380f1c071a233ec4ca751e4d0eccd0edc470936
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-03-19 14:00:00 +0300

    model user method author_of? spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,21 @@ RSpec.describe User, type: :model do
 
   it { should have_many(:authored_questions).class_name('Question') }
   it { should have_many(:authored_answers).class_name('Answer') }
+
+  describe 'public instance methods' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question, author: user) }
+
+    context 'responds to its methods' do
+      it { expect(user).to respond_to(:author_of?) }
+    end
+
+    context 'executes methods correctly' do
+      context '#author_of?' do
+        it "does what it's supposed to..." do
+          expect(user).to be_author_of(question)
+        end
+      end
+    end
+  end
 end
