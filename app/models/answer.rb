766af8774bbfe0b85ce915435b6c869c5a2457ed
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-03-25 00:35:12 +0300

    answers + files

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,8 @@
 class Answer < ApplicationRecord
   default_scope { order(best: :desc) }
 
+  has_many_attached :files
+
   belongs_to :question
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
