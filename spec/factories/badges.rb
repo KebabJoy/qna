d0c2ff59b76f34d5f7fe0799c8efbf41d526f458
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-03-31 21:34:48 +0300

    added bootsrap + badges mechanic

--- spec/factories/badges.rb
+++ spec/factories/badges.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :badge do
-    name { "MyString" }
-    img_url { "MyString" }
+    name { 'My badge' }
+    img_url { 'https://www.pngkey.com/png/full/17-175787_red-seal-badge-transparent-png-clip-art-banner.png' }
   end
 end
