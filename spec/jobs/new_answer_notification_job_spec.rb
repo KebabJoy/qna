2855943a984b02fefdc3b4eff8945d5836306456
KebabJoy <50750550+KebabJoy@users.noreply.github.com>
2021-04-21 23:58:46 +0300

    job test

--- spec/jobs/new_answer_notification_job_spec.rb
+++ spec/jobs/new_answer_notification_job_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe NewAnswerNotificationJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:service) { double('NewAnswerNotification') }
+  let(:question) { create(:question, author: create(:user)) }
+  let(:answer) { create(:answer, question: question) }
+
+
+  before do
+    allow(NewAnswerNotification).to receive(:new).and_return(service)
+  end
+
+  it 'calls NewAnswerNotification#send_new_answer_notification' do
+    expect(service).to receive(:send_new_answer_notification)
+    NewAnswerNotificationJob.perform_now(question, answer)
+  end
 end
